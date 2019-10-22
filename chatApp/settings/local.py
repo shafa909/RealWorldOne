ed9fb12ec91dfb9cebf1fa5ec7ec20eadd7131f9
shafa909 <shafa909@gmail.com>
2019-10-22 14:13:46 +0530

    adding development db

--- chatApp/settings/local.py
+++ chatApp/settings/local.py
@@ -4,3 +4,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
--- chatApp/settings/prod.py
+++ chatApp/settings/prod.py
@@ -3,3 +3,16 @@ from .base import *
 DEBUG = False
 
 ALLOWED_HOSTS = []
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'myproject',
+        'USER': 'myprojectuser',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
+
